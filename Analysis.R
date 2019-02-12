c1656f37076ccc852a3dd7a872b77036a412306a
Geraldi Gunawan <geraldigunawan23@gmail.com>
2019-02-12 17:08:13 +1100

    Update
    Contains exercises

--- Analysis.R
+++ Analysis.R
@@ -75,3 +75,31 @@ str(longlat)
 any(is.na(longlat$latitude))
 
 rm(name)
+
+#Exercise 1: Get prices for all private room with minimum stay of 1 night
+onenight <- subset(listings_summary, minimum_nights ==1, select=c(room_type,price,minimum_nights))
+str(onenight)
+onenight_private <- onenight[grep("rivate",onenight$room_type),]
+str(onenight_private)
+#order price ascending
+onenight_private_price <- onenight_private[order(onenight_private$price),]
+#order price descending
+onenight_private_price <- onenight_private[order(-onenight_private$price),]
+str(onenight_private_price)
+
+#Exercise 2: Classify price > 150 as high, price in between 60 to 149 as middle, below 60 as low
+onenight_private_price$grade[onenight_private_price$price >= 170] <- "High"
+onenight_private_price$grade[onenight_private_price$price <= 169 & onenight_private_price$price >= 60 ] <- "Middle"
+onenight_private_price$grade[onenight_private_price$price < 60 ] <- "Low"
+onenight_private_price
+tail(onenight_private_price)
+str(onenight_private_price)
+count_grade <- as.data.frame(table(onenight_private_price$grade))
+count_grade
+colnames(count_grade)[colnames(count_grade)=="Var1"] <- "Grade"
+colnames(count_grade)[colnames(count_grade)=="Freq"] <- "Total"
+listing_per_grade <- ggplot(count_grade, aes(x=factor(Grade), y = Total)) + geom_bar(stat="identity") + scale_x_discrete(limits=c("Low", "Middle", "High"))
+listing_per_grade
+jpeg('Diagrams/listing_per_grade.jpg')
+plot(listing_per_grade)
+dev.off()
